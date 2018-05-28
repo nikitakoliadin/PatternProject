20cf13291c972d536abfdaa685d1bd6df836a530
Niktia Koliadin <qThegamEp@gmail.com>
2018-05-28 18:33:37 +0300

    Added math functions util class

---
+++ src/main/java/com/patternproject/util/MathFunctionsUtil.java
@@ -0,0 +1,43 @@
+package com.patternproject.util;
+
+import java.util.ArrayList;
+
+/**
+ * @author Koliadin Nikita
+ */
+public class MathFunctionsUtil {
+
+    /**
+     * This constant contains a list of functions available for use in the calculator
+     */
+    public static final ArrayList<String> MATH_FUNCTIONS = new ArrayList<>(){{
+        add("abs");
+        add("acos");
+        add("asin");
+        add("atan");
+        add("cbrt");
+        add("ceil");
+        add("cos");
+        add("cosh");
+        add("exp");
+        add("expm1");
+        add("floor");
+        add("getExponent");
+        add("log");
+        add("log10");
+        add("log1p");
+        add("nextDown");
+        add("nextUp");
+        add("rint");
+        add("round");
+        add("signum");
+        add("sin");
+        add("sinh");
+        add("sqrt");
+        add("tan");
+        add("tanh");
+        add("toDegrees");
+        add("toRadians");
+        add("ulp");
+    }};
+}
