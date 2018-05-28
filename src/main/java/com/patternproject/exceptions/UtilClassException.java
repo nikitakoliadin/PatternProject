f9bd511405106c081d802dcdc3562ea59bf68602
Niktia Koliadin <qThegamEp@gmail.com>
2018-05-28 19:30:17 +0300

    Added new runtime exception

---
+++ src/main/java/com/patternproject/exceptions/UtilClassException.java
@@ -0,0 +1,11 @@
+package com.patternproject.exceptions;
+
+/**
+ * @author Koliadin Nikita
+ */
+public class UtilClassException extends RuntimeException {
+
+    public UtilClassException(String message) {
+        super(message);
+    }
+}
