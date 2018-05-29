57bbc424854df8d2b77245bd320f3765578a536d
Niktia Koliadin <qThegamEp@gmail.com>
2018-05-29 19:03:26 +0300

    Changed double brace initialization to static block initialization

--- src/main/java/com/patternproject/util/MathFunctionsUtil.java
+++ src/main/java/com/patternproject/util/MathFunctionsUtil.java
@@ -12,42 +12,44 @@ import java.util.List;
  */
 public final class MathFunctionsUtil {
 
+    /**
+     * This constant contains a list of functions available for use in the calculator
+     */
+    public static final List<String> MATH_FUNCTIONS = new ArrayList<>();
+
+    static {
+        MATH_FUNCTIONS.add("abs");
+        MATH_FUNCTIONS.add("acos");
+        MATH_FUNCTIONS.add("asin");
+        MATH_FUNCTIONS.add("atan");
+        MATH_FUNCTIONS.add("cbrt");
+        MATH_FUNCTIONS.add("ceil");
+        MATH_FUNCTIONS.add("cos");
+        MATH_FUNCTIONS.add("cosh");
+        MATH_FUNCTIONS.add("exp");
+        MATH_FUNCTIONS.add("expm1");
+        MATH_FUNCTIONS.add("floor");
+        MATH_FUNCTIONS.add("getExponent");
+        MATH_FUNCTIONS.add("log");
+        MATH_FUNCTIONS.add("log10");
+        MATH_FUNCTIONS.add("log1p");
+        MATH_FUNCTIONS.add("nextDown");
+        MATH_FUNCTIONS.add("nextUp");
+        MATH_FUNCTIONS.add("rint");
+        MATH_FUNCTIONS.add("round");
+        MATH_FUNCTIONS.add("signum");
+        MATH_FUNCTIONS.add("sin");
+        MATH_FUNCTIONS.add("sinh");
+        MATH_FUNCTIONS.add("sqrt");
+        MATH_FUNCTIONS.add("tan");
+        MATH_FUNCTIONS.add("tanh");
+        MATH_FUNCTIONS.add("toDegrees");
+        MATH_FUNCTIONS.add("toRadians");
+        MATH_FUNCTIONS.add("ulp");
+    }
+
     @Contract(" -> fail")
     private MathFunctionsUtil() {
         throw new UtilClassException("Can not create object of util class");
     }
-
-    /**
-     * This constant contains a list of functions available for use in the calculator
-     */
-    public static final List<String> MATH_FUNCTIONS = new ArrayList<>(){{
-        add("abs");
-        add("acos");
-        add("asin");
-        add("atan");
-        add("cbrt");
-        add("ceil");
-        add("cos");
-        add("cosh");
-        add("exp");
-        add("expm1");
-        add("floor");
-        add("getExponent");
-        add("log");
-        add("log10");
-        add("log1p");
-        add("nextDown");
-        add("nextUp");
-        add("rint");
-        add("round");
-        add("signum");
-        add("sin");
-        add("sinh");
-        add("sqrt");
-        add("tan");
-        add("tanh");
-        add("toDegrees");
-        add("toRadians");
-        add("ulp");
-    }};
 }
