8ccc3f9712ef6db47e309664e69bb685ea4aa4bf
Niktia Koliadin <qThegamEp@gmail.com>
2018-05-30 17:35:38 +0300

    Added test of immutable list exception

--- src/test/java/com/patternproject/util/MathFunctionsUtilTest.java
+++ src/test/java/com/patternproject/util/MathFunctionsUtilTest.java
@@ -6,8 +6,7 @@ import lombok.val;
 import org.junit.Test;
 
 import java.lang.reflect.InvocationTargetException;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -18,13 +17,20 @@ public class MathFunctionsUtilTest {
 
     @Test
     public void shouldBeCorrectListOfMathFunctions() {
-        val expected = new ArrayList<>(Arrays.asList(
-                "abs", "acos", "asin", "atan", "cbrt", "ceil", "cos", "cosh", "exp", "expm1",
-                "floor", "getExponent", "log", "log10", "log1p", "nextDown", "nextUp", "rint",
-                "round", "signum", "sin", "sinh", "sqrt", "tan", "tanh", "toDegrees", "toRadians", "ulp")
+        val expected = List.of(
+                "abs", "acos", "asin", "atan", "cbrt", "ceil", "cos", "cosh", "exp", "expm1", "floor",
+                "getExponent", "log", "log10", "log1p", "nextDown", "nextUp", "rint", "round", "signum",
+                "sin", "sinh", "sqrt", "tan", "tanh", "toDegrees", "toRadians", "ulp"
         );
 
         assertThat(MathFunctionsUtil.MATH_FUNCTIONS).isNotNull().hasSize(28).isEqualTo(expected);
+    }
+
+    @Test
+    public void shouldThrowUnsupportedOperationExceptionWhenDoPutOperationsWithImmutableList() {
+        assertThatExceptionOfType(UnsupportedOperationException.class).isThrownBy(
+                () -> MathFunctionsUtil.MATH_FUNCTIONS.add("a")
+        );
     }
 
     @Test
