321fe4a7ea71f7f3427ae2f1762efa00a1e97e57
Niktia Koliadin <qThegamEp@gmail.com>
2018-05-30 17:28:06 +0300

    Changed static block initialization to factory methods for immutable list

--- src/main/java/com/patternproject/util/MathFunctionsUtil.java
+++ src/main/java/com/patternproject/util/MathFunctionsUtil.java
@@ -4,7 +4,6 @@ import com.patternproject.exceptions.UtilClassException;
 
 import org.jetbrains.annotations.Contract;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -13,40 +12,13 @@ import java.util.List;
 public final class MathFunctionsUtil {
 
     /**
-     * This constant contains a list of functions available for use in the calculator
+     * This constant contains a immutable list of functions available for use in the calculator
      */
-    public static final List<String> MATH_FUNCTIONS = new ArrayList<>();
-
-    static {
-        MATH_FUNCTIONS.add("abs");
-        MATH_FUNCTIONS.add("acos");
-        MATH_FUNCTIONS.add("asin");
-        MATH_FUNCTIONS.add("atan");
-        MATH_FUNCTIONS.add("cbrt");
-        MATH_FUNCTIONS.add("ceil");
-        MATH_FUNCTIONS.add("cos");
-        MATH_FUNCTIONS.add("cosh");
-        MATH_FUNCTIONS.add("exp");
-        MATH_FUNCTIONS.add("expm1");
-        MATH_FUNCTIONS.add("floor");
-        MATH_FUNCTIONS.add("getExponent");
-        MATH_FUNCTIONS.add("log");
-        MATH_FUNCTIONS.add("log10");
-        MATH_FUNCTIONS.add("log1p");
-        MATH_FUNCTIONS.add("nextDown");
-        MATH_FUNCTIONS.add("nextUp");
-        MATH_FUNCTIONS.add("rint");
-        MATH_FUNCTIONS.add("round");
-        MATH_FUNCTIONS.add("signum");
-        MATH_FUNCTIONS.add("sin");
-        MATH_FUNCTIONS.add("sinh");
-        MATH_FUNCTIONS.add("sqrt");
-        MATH_FUNCTIONS.add("tan");
-        MATH_FUNCTIONS.add("tanh");
-        MATH_FUNCTIONS.add("toDegrees");
-        MATH_FUNCTIONS.add("toRadians");
-        MATH_FUNCTIONS.add("ulp");
-    }
+    public static final List<String> MATH_FUNCTIONS = List.of(
+            "abs", "acos", "asin", "atan", "cbrt", "ceil", "cos", "cosh", "exp", "expm1", "floor",
+            "getExponent", "log", "log10", "log1p", "nextDown", "nextUp", "rint", "round", "signum",
+            "sin", "sinh", "sqrt", "tan", "tanh", "toDegrees", "toRadians", "ulp"
+    );
 
     @Contract(" -> fail")
     private MathFunctionsUtil() {
