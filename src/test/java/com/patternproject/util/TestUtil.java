3df0ec28b92c37e79442c8aed575f55c93de9351
Niktia Koliadin <qThegamEp@gmail.com>
2018-06-02 15:42:20 +0300

    Added TestUtil class

---
+++ src/test/java/com/patternproject/util/TestUtil.java
@@ -0,0 +1,20 @@
+package com.patternproject.util;
+
+import java.io.InputStream;
+import java.io.PrintStream;
+
+/**
+ * @author Koliadin Nikita
+ */
+public class TestUtil {
+
+    /**
+     * This constant contains default value of System.in
+     */
+    public static final InputStream CONSOLE_INPUT_STREAM = System.in;
+
+    /**
+     * This constant contains default value of System.out
+     */
+    public static final PrintStream CONSOLE_PRINT_STREAM = System.out;
+}
