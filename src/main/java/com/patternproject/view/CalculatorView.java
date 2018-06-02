63b2c691f121d89c75d35e7b2b3b4f3386dba322
Niktia Koliadin <qThegamEp@gmail.com>
2018-06-02 19:28:50 +0300

    Added view interface

---
+++ src/main/java/com/patternproject/view/CalculatorView.java
@@ -0,0 +1,14 @@
+package com.patternproject.view;
+
+/**
+ * @author Koliadin Nikita
+ */
+@FunctionalInterface
+public interface CalculatorView {
+
+    /**
+     * This method should be a default implementation of the preparation
+     * and start of the console calculator
+     */
+    void run();
+}
