7ea31819ae036ecdc42f7a1cabd7ae81629f3727
Niktia Koliadin <qThegamEp@gmail.com>
2018-06-05 19:32:29 +0300

    Added log

--- src/main/java/com/patternproject/Application.java
+++ src/main/java/com/patternproject/Application.java
@@ -4,13 +4,17 @@ import com.patternproject.model.CalculatorNashornModel;
 import com.patternproject.view.CalculatorConsoleView;
 import com.patternproject.controller.CalculatorConsoleController;
 
+import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 
 /**
  * @author Koliadin Nikita
  */
+@Slf4j
 public class Application {
     public static void main(String[] args) {
+        log.info("Preparing application to start...");
+
         val calculatorModel = new CalculatorNashornModel();
         val calculatorController = new CalculatorConsoleController();
         val calculatorView =  new CalculatorConsoleView();
@@ -18,6 +22,8 @@ public class Application {
         calculatorController.setCalculatorModel(calculatorModel);
         calculatorController.setCalculatorView(calculatorView);
         calculatorView.setCalculatorController(calculatorController);
+
+        log.info("Preparing was done successful!");
 
         calculatorView.run();
     }
