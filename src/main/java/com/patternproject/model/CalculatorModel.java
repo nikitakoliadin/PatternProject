a90e760235d82f15c4551dc47ce604e44788f014
Niktia Koliadin <qThegamEp@gmail.com>
2018-06-06 10:39:43 +0300

    Refracted code style

--- src/main/java/com/patternproject/model/CalculatorModel.java
+++ src/main/java/com/patternproject/model/CalculatorModel.java
@@ -9,6 +9,7 @@ public interface CalculatorModel {
     /**
      * This method implement the calculation of the expression
      * that comes as a string.
+     *
      * @param expression is input parameter that can have math functions
      *                   and  any kind of math operations.
      * @return double value after calculation of the expression.
