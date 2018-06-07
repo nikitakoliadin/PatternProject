2152ccd2dac9c0d4a81f1f7511acc0a796219557
Niktia Koliadin <qThegamEp@gmail.com>
2018-06-07 19:13:14 +0300

    Refracted log

--- src/main/java/com/patternproject/Application.java
+++ src/main/java/com/patternproject/Application.java
@@ -20,6 +20,7 @@ public class Application {
                 + System.lineSeparator()
                 + "-------------------------------------------------------------------------"
         );
+        log.info("Method [{}] was started successful", new Object() {}.getClass().getEnclosingMethod().getName());
 
         val calculatorModel = new CalculatorNashornModel();
         val calculatorController = new CalculatorConsoleController();
