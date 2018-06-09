ad4c6fa7129488328e427e3cfca3a9a6ee8fb5e0
Niktia Koliadin <qThegamEp@gmail.com>
2018-06-09 17:04:05 +0300

    Refracted class, added controller methods, edited JavaDoc, Renamed class

---
+++ src/main/java/com/patternproject/controller/CalculatorControllerImpl.java
@@ -0,0 +1,112 @@
+package com.patternproject.controller;
+
+import com.patternproject.model.CalculatorModel;
+import com.patternproject.view.CalculatorView;
+
+import lombok.*;
+import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.Reader;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.stream.Stream;
+import java.util.function.DoubleConsumer;
+
+/**
+ * @author Koliadin Nikita
+ */
+@Slf4j
+@NoArgsConstructor
+@AllArgsConstructor
+public class CalculatorControllerImpl implements CalculatorController {
+
+    @NonNull
+    @Getter
+    @Setter
+    private CalculatorModel calculatorModel;
+    @NonNull
+    @Getter
+    @Setter
+    private CalculatorView calculatorView;
+
+    /**
+     * This method implements the beginning of work with the calculator. It does not have any input parameters.
+     * The output is carried out to the console, the reading is done from the console also.
+     * If expression divided by ';' then each result out to the new line. Empty lines are not calculated.
+     */
+    @Override
+    public void startDefaultCalculate() {
+        log.info("Preparing double consumer to set default output to the console for the calculator");
+
+        DoubleConsumer doubleConsumer = System.out::println;
+
+        log.info("Preparing double consumer to set default output to the console for the calculator " +
+                "was done successful");
+
+        calculateFromConsoleTo(doubleConsumer);
+    }
+
+    /**
+     * This method takes Reader parameter and after calculation of the expression out the result to console
+     * in the new line. If expression divided by ';' then each result out to the new line.
+     * Empty lines are not calculated.
+     *
+     * @param reader information reading parameters.
+     */
+    public void calculateToConsoleFrom(Reader reader) {
+        log.info("Preparing double consumer to set default output to the console for the calculator");
+
+        DoubleConsumer doubleConsumer = System.out::println;
+
+        log.info("Preparing double consumer to set default output to the console for the calculator " +
+                "was done successful");
+
+        calculate(reader, doubleConsumer);
+    }
+
+    /**
+     * This method takes DoubleConsumer parameter and after calculation of the expression out the result
+     * to DoubleConsumer. Empty lines are not calculated.
+     *
+     * @param resultConsumer result output parameter.
+     */
+    public void calculateFromConsoleTo(DoubleConsumer resultConsumer) {
+        log.info("Preparing input stream reader to set default input to the console for the calculator");
+
+        try (val reader = new InputStreamReader(System.in, "UTF-8")) {
+            log.info("Preparing input stream reader [{}] to set default input to the console for the " +
+                            "calculator with encoding [{}] was created successful",
+                    reader.getClass(),
+                    reader.getEncoding()
+            );
+            calculate(reader, resultConsumer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * This method read expression to calculate from Reader and then output result to DoubleConsumer.
+     * Empty lines are not calculated.
+     *
+     * @param reader         information reading parameters.
+     * @param resultConsumer result output parameter.
+     */
+    public void calculateFromTo(Reader reader, DoubleConsumer resultConsumer) {
+        log.info("Preparing calculator to work!");
+
+        calculate(reader, resultConsumer);
+    }
+
+    private void calculate(@NotNull Reader reader, @NotNull DoubleConsumer resultConsumer) {
+        log.info("Preparing calculator to work was done successful! Waiting for user input");
+
+        new BufferedReader(reader).lines()
+                .flatMap((s) -> Stream.of(s.split(";")))
+                .filter((s) -> !s.trim().isEmpty())
+                .mapToDouble(calculatorModel::calculate)
+                .forEach(resultConsumer);
+    }
+}
