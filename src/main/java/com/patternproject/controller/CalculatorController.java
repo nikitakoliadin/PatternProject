8a46f7ce2872ec1fc22b5f63b6e9d5a4a8562796
Niktia Koliadin <qThegamEp@gmail.com>
2018-06-09 17:34:57 +0300

    Added method to the interface

--- src/main/java/com/patternproject/controller/CalculatorController.java
+++ src/main/java/com/patternproject/controller/CalculatorController.java
@@ -1,14 +1,40 @@
 package com.patternproject.controller;
 
+import java.io.Reader;
+import java.util.function.DoubleConsumer;
+
 /**
  * @author Koliadin Nikita
  */
-@FunctionalInterface
 public interface CalculatorController {
 
     /**
-     * This method should be a standard implementation of the controller
-     * that parses the incoming data and outputs the result.
+     * This method should be a standard implementation of the calculator that parses the incoming data and
+     * outputs the result. Input and output comas from console.
      */
     void startDefaultCalculate();
+
+    /**
+     * This method should take Reader parameter and after calculation of the expression out the result to console
+     * in the new line.
+     *
+     * @param reader information reading parameters.
+     */
+    void calculateToConsoleFrom(Reader reader);
+
+    /**
+     * This method should take DoubleConsumer parameter and after calculation of the expression out the result
+     * to DoubleConsumer. Empty lines are not calculated.
+     *
+     * @param resultConsumer result output parameter.
+     */
+    void calculateFromConsoleTo(DoubleConsumer resultConsumer);
+
+    /**
+     * This method read expression to calculate from Reader and then output result to DoubleConsumer.
+     *
+     * @param reader         information reading parameters.
+     * @param resultConsumer result output parameter.
+     */
+    void calculateFromTo(Reader reader, DoubleConsumer resultConsumer);
 }
