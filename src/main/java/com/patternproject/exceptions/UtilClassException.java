ae107e7175111618ce9f6faea0950838964d6148
Niktia Koliadin <qThegamEp@gmail.com>
2018-06-09 18:34:57 +0300

    Edited JavaDoc

--- src/main/java/com/patternproject/exceptions/UtilClassException.java
+++ src/main/java/com/patternproject/exceptions/UtilClassException.java
@@ -3,8 +3,7 @@ package com.patternproject.exceptions;
 /**
  * @author Koliadin Nikita
  *
- * This exception should be thrown when there was an attempt to create
- * a util object using reflection.
+ * This exception should be thrown when there was an attempt to create a util object using reflection.
  */
 public class UtilClassException extends RuntimeException {
 
