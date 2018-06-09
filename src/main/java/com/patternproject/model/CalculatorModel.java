40a8a9f62a63d33978c1f071c3ae9224fb4c26c3
Niktia Koliadin <qThegamEp@gmail.com>
2018-06-09 18:35:30 +0300

    Edited JavaDoc

--- src/main/java/com/patternproject/model/CalculatorModel.java
+++ src/main/java/com/patternproject/model/CalculatorModel.java
@@ -7,11 +7,9 @@ package com.patternproject.model;
 public interface CalculatorModel {
 
     /**
-     * This method implement the calculation of the expression
-     * that comes as a string.
+     * This method implement the calculation of the expression that comes as a string.
      *
-     * @param expression is input parameter that can have math functions
-     *                   and  any kind of math operations.
+     * @param expression is input parameter that can have math functions and  any kind of math operations.
      * @return double value after calculation of the expression.
      */
     double calculate(String expression);
