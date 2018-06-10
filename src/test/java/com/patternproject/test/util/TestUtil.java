4f977cce301879975d06e19c5833773ee52af80e
Niktia Koliadin <qThegamEp@gmail.com>
2018-06-10 17:25:18 +0300

    Added lombok annotation of util class

--- src/test/java/com/patternproject/test/util/TestUtil.java
+++ src/test/java/com/patternproject/test/util/TestUtil.java
@@ -1,20 +1,23 @@
 package com.patternproject.test.util;
 
+import lombok.experimental.UtilityClass;
+
 import java.io.InputStream;
 import java.io.PrintStream;
 
 /**
  * @author Koliadin Nikita
  */
+@UtilityClass
 public class TestUtil {
 
     /**
      * This constant contains default value of System.in
      */
-    public static final InputStream CONSOLE_INPUT_STREAM = System.in;
+    public final InputStream CONSOLE_INPUT_STREAM = System.in;
 
     /**
      * This constant contains default value of System.out
      */
-    public static final PrintStream CONSOLE_PRINT_STREAM = System.out;
+    public final PrintStream CONSOLE_PRINT_STREAM = System.out;
 }
