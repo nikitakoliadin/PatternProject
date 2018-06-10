ce4a77fdc5e14794b7abcc40398d0db15b830df8
Niktia Koliadin <qThegamEp@gmail.com>
2018-06-10 12:58:38 +0300

    Added lombok annotation of util class

--- src/main/java/com/patternproject/util/MathFunctionsUtil.java
+++ src/main/java/com/patternproject/util/MathFunctionsUtil.java
@@ -1,30 +1,21 @@
 package com.patternproject.util;
 
-import com.patternproject.exceptions.UtilClassException;
-
-import lombok.extern.slf4j.Slf4j;
-import org.jetbrains.annotations.Contract;
+import lombok.experimental.UtilityClass;
 
 import java.util.List;
 
 /**
  * @author Koliadin Nikita
  */
-@Slf4j
-public final class MathFunctionsUtil {
+@UtilityClass
+public class MathFunctionsUtil {
 
     /**
      * This constant contains a immutable list of functions available for use in the calculator.
      */
-    public static final List<String> MATH_FUNCTIONS = List.of(
+    public final List<String> MATH_FUNCTIONS = List.of(
             "abs", "acos", "asin", "atan", "cbrt", "ceil", "cos", "cosh", "exp", "expm1", "floor",
             "getExponent", "log", "log10", "log1p", "nextDown", "nextUp", "rint", "round", "signum",
             "sin", "sinh", "sqrt", "tan", "tanh", "toDegrees", "toRadians", "ulp"
     );
-
-    @Contract(" -> fail")
-    private MathFunctionsUtil() {
-        log.error("Can not create object of util class");
-        throw new UtilClassException("Can not create object of util class");
-    }
 }
