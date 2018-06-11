14839081c919d33a85144c6edc9634dda2ec81bf
Niktia Koliadin <qThegamEp@gmail.com>
2018-06-11 19:25:46 +0300

    Changed constant variables

--- src/test/java/com/patternproject/test/util/TestUtil.java
+++ src/test/java/com/patternproject/test/util/TestUtil.java
@@ -11,22 +11,16 @@ import java.io.PrintStream;
 @UtilityClass
 public class TestUtil {
 
-    /**
-     * This constant contain InputStream to console.
-     */
-    public final InputStream CONSOLE_INPUT_STREAM = System.in;
+    private final InputStream consoleInputStream = System.in;
+
+    private final PrintStream consolePrintStream = System.out;
 
     /**
-     * This constant contain PrintStream to console.
-     */
-    public final PrintStream CONSOLE_PRINT_STREAM = System.out;
-
-    /**
-     * This method set input and output to the console. Use it before and after each test where the input
+     * This method set input and output to the console. It is used before and after each test where the input
      * or output is substituted.
      */
-    public void setInputOutputToConsole() {
-        System.setIn(TestUtil.CONSOLE_INPUT_STREAM);
-        System.setOut(TestUtil.CONSOLE_PRINT_STREAM);
+    public void setInputOutputStreamToConsole() {
+        System.setIn(consoleInputStream);
+        System.setOut(consolePrintStream);
     }
 }
