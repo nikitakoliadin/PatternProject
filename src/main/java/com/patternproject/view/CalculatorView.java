d8ce25e1c16b8ca3ebb1f3416d5e1804092f3959
Niktia Koliadin <qThegamEp@gmail.com>
2018-06-12 14:47:29 +0300

    Added methods, edited JavaDoc

--- src/main/java/com/patternproject/view/CalculatorView.java
+++ src/main/java/com/patternproject/view/CalculatorView.java
@@ -1,11 +1,23 @@
 package com.patternproject.view;
 
+import com.patternproject.controller.CalculatorController;
+
 import java.util.function.DoubleConsumer;
 
 /**
  * @author Koliadin Nikita
  */
 public interface CalculatorView {
+
+    /**
+     * @return parameter of the calculator controller
+     */
+    CalculatorController getCalculatorController();
+
+    /**
+     * @param calculatorController parameter of the calculator controller
+     */
+    void setCalculatorController(CalculatorController calculatorController);
 
     /**
      * This method should be a default implementation of the preparation and start of the console calculator.
