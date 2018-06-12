f323e36bcd0e07f2d769e85327ad3b6452dee7f7
Niktia Koliadin <qThegamEp@gmail.com>
2018-06-12 13:42:23 +0300

    Added new output method

--- src/main/java/com/patternproject/view/CalculatorView.java
+++ src/main/java/com/patternproject/view/CalculatorView.java
@@ -1,13 +1,22 @@
 package com.patternproject.view;
 
+import java.util.function.DoubleConsumer;
+
 /**
  * @author Koliadin Nikita
  */
-@FunctionalInterface
 public interface CalculatorView {
 
     /**
      * This method should be a default implementation of the preparation and start of the console calculator.
      */
     void startConsoleCalculator();
+
+    /**
+     * This method should outputs the result.
+     *
+     * @param resultConsumer result output parameter.
+     * @param result         result of the calculation.
+     */
+    void outputResult(DoubleConsumer resultConsumer, double result);
 }
