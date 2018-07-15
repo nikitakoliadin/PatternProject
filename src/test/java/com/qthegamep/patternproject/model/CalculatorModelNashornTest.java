067ecf60a6f45f915e724ae8d9c2fb6461c4b1d3
Nikita Koliadin <qThegamEp@gmail.com>
2018-07-15 16:08:55 +0300

    Deleted unnecessary @author JavaDoc tag

--- src/test/java/com/qthegamep/patternproject/model/CalculatorModelNashornTest.java
+++ src/test/java/com/qthegamep/patternproject/model/CalculatorModelNashornTest.java
@@ -16,9 +16,6 @@ import javax.script.ScriptException;
 
 import static org.assertj.core.api.Assertions.*;
 
-/**
- * @author Koliadin Nikita
- */
 public class CalculatorModelNashornTest {
 
     @ClassRule
