a240a2566679aad266aa83eda2d37fa290eaaee7
Nikita Koliadin <qThegamEp@gmail.com>
2018-07-17 11:39:39 +0300

    Added JavaDoc

--- src/main/java/com/qthegamep/patternproject/controller/CalculatorController.java
+++ src/main/java/com/qthegamep/patternproject/controller/CalculatorController.java
@@ -6,6 +6,11 @@ import com.qthegamep.patternproject.view.CalculatorView;
 import java.io.Reader;
 import java.util.function.DoubleConsumer;
 
+/**
+ * This class is a calculator controller.
+ * It should have a standard console implementation of the calculator,
+ * as well as a customizable implementation of the calculator.
+ */
 public interface CalculatorController {
 
     /**
